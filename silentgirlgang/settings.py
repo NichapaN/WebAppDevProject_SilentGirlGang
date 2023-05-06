19f786edaec8950aab9fa19b2001adb1840a5a07
SirapatPK <122334903+SirapatPK@users.noreply.github.com>
2023-05-06 20:53:54 +0700

    update tpopstore template

--- silentgirlgang/settings.py
+++ silentgirlgang/settings.py
@@ -13,23 +13,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django
+from django.utils.encoding import force_str
+django.utils.encoding.force_text = force_str
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-&696=spqfj@e#ax(hjy+@s8z9%+y%mx#kdo_ry3hv2mfgn!^(y'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -40,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tpopstore',
     'basket',
+    # 'django_countries',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- tpopstore/views.py
+++ tpopstore/views.py
@@ -3,11 +3,6 @@ from django.shortcuts import get_object_or_404, render
 from .models import *
 
 
-# def categories(request):
-#     return {
-#         'categories': Category.objects.all()
-#     }
-
 
 def product_all(request):
     products = Product.products.all()
