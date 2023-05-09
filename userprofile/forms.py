f783f0eb16a766b72bba723874745e8b5fe68a41
Nicharee Chuphanich <122334848+NichareeChu@users.noreply.github.com>
2023-05-10 00:35:10 +0700

    customer login

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -3,7 +3,7 @@ from django import forms
 #from django.contrib.auth import get_user_model
 #User = get_user_model()
 from .models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 
@@ -20,7 +20,7 @@ class CustomerSignUpForm(UserCreationForm):
         fields = ['username','first_name', 'last_name', 'email', 'phone', 'password1', 'password2',]
 
 class AgencySignUpForm(UserCreationForm):
-    agency_name = forms.EmailField(max_length=255, required=True)
+    agency_name = forms.CharField(max_length=255, required=True)
     email = forms.EmailField(max_length=255, required=True)
     phone = forms.CharField(max_length=10, required=True)
     
@@ -28,3 +28,16 @@ class AgencySignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'agency_name', 'email', 'phone', 'password1', 'password2',]
+
+
+class UserLoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'login-username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'id': 'login-pwd',
+        }
+    ))
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,12 +2,14 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
-#from .forms import UserLoginForm
+from .forms import UserLoginForm
 
 app_name = 'userprofile'
 
 urlpatterns = [
     path('customersignup/', views.customersignup, name='customersignup'),
     path('agencysignup/', views.agencysignup, name='agencysignup'),
+    path('customerlogin/', auth_views.LoginView.as_view(template_name='account/customerlogin.html',
+                                                form_class=UserLoginForm), name='customerlogin'),
 ]
     
