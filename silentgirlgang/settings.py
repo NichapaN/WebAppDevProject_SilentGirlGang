b7de2bebfc3f59298753a6ba5cad1123d78f1c36
NichapaN <122334912+NichapaN@users.noreply.github.com>
2023-05-09 19:08:21 +0700

    add userprofile(empty)

--- silentgirlgang/settings.py
+++ silentgirlgang/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'userprofile',
     'tpopstore',
     'basket',
     'vendors',
@@ -133,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+AUTH_USER_MODEL = 'userprofile.User'
